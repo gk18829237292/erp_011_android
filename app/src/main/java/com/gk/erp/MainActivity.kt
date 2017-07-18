dce407bbdb22884117b2fa940ac6e1a54a226d37
ke.gao <ke.gao@hobot.cc>
2017-07-18 19:53:04 +0800

    dev init

--- app/src/main/java/com/gk/erp/MainActivity.kt
+++ app/src/main/java/com/gk/erp/MainActivity.kt
@@ -14,8 +14,4 @@ class MainActivity : BaseActivity() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
